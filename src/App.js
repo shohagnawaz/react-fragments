bceb712ccc66efd220480d020190427b181032e3
shohagnawaz <snawazdigital@gmail.com>
2020-10-31 02:08:07 +0600

    fragments

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Fragdemo from './Components/Fragdemo';
+
+function App() {
+  return (
+    <div className="App">
+      <Fragdemo></Fragdemo>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Fragdemo.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+class Fragdemo extends Component {
+    render () {
+        return (
+            <Fragment>
+                <h1>Md. Shah Nawaz Shohag</h1>
+                <h1>Md. Shah Nawaz Shohag</h1>
+                <h1>Md. Shah Nawaz Shohag</h1>
+            </Fragment>
+        );
+    }
+}
+
+export default Fragdemo;
